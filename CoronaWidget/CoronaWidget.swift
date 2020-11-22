ad948ef5bb6ac2f33227b25f0c5a270397a1d755
Engin Kurutepe <engin@kurutepe.com>
2020-11-22 19:09:35 +0100

    adapt to widget size

--- CoronaWidget/CoronaWidget.swift
+++ CoronaWidget/CoronaWidget.swift
@@ -11,11 +11,11 @@ import Intents
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent())
+        SimpleEntry(date: Date(), configuration: ConfigurationIntent(), size: context.displaySize)
     }
 
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration)
+        let entry = SimpleEntry(date: Date(), configuration: configuration, size: context.displaySize)
         completion(entry)
     }
 
@@ -26,7 +26,7 @@ struct Provider: IntentTimelineProvider {
         let currentDate = Date()
         for hourOffset in 0 ..< 5 {
             let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, configuration: configuration)
+            let entry = SimpleEntry(date: entryDate, configuration: configuration, size: context.displaySize)
             entries.append(entry)
         }
 
@@ -38,6 +38,7 @@ struct Provider: IntentTimelineProvider {
 struct SimpleEntry: TimelineEntry {
     let date: Date
     let configuration: ConfigurationIntent
+    let size: CGSize
 }
 
 struct CoronaWidgetEntryView : View {
@@ -62,7 +63,7 @@ struct CoronaWidgetEntryView : View {
             Text(entry.date, style: .time)
             Spacer()
         }
-        .background(RadialGradient(gradient: Gradient(colors: [Color.white, Color.gray]), center: .center, startRadius: 20, endRadius: 160))
+        .background(RadialGradient(gradient: Gradient(colors: [Color.white, Color.gray]), center: .center, startRadius: 0.2*entry.size.width, endRadius: 0.8*entry.size.width))
     }
 }
 
@@ -82,13 +83,13 @@ struct CoronaWidget: Widget {
 struct CoronaWidget_Previews: PreviewProvider {
     static var previews: some View {
         Group {
-            CoronaWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            CoronaWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), size: CGSize(width: 180, height: 180)))
                 .previewContext(WidgetPreviewContext(family: .systemSmall))
 
-            CoronaWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            CoronaWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), size: CGSize(width: 360, height: 180)))
                 .previewContext(WidgetPreviewContext(family: .systemMedium))
 
-            CoronaWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+            CoronaWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), size: CGSize(width: 360, height: 360)))
                 .previewContext(WidgetPreviewContext(family: .systemLarge))
 
         }
